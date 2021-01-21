f1dd3febf51492e740136e714644a4f56ddc7f88
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2021-01-21 01:04:28 -0500

    Update exercises.test.js

--- exercises.test.js
+++ exercises.test.js
@@ -7,68 +7,72 @@ const {
 
 describe('bubbleSort', () => {
     it('sorts a numeric list of positive numbers', () => {
+        const numbersOG = [131, 174, 5, 10, 145, 143, 63, 108, 170, 81, 170, 5];
         const numbers = [131, 174, 5, 10, 145, 143, 63, 108, 170, 81, 170, 5];
-        const sorted = numbers.sort((a, b) => a - b); // We'll use the built-in sort for comparison
+        numbers.sort((a, b) => a - b); // We'll use the built-in sort for comparison
 
-        expect(bubbleSort(numbers)).toEqual(sorted);
+        expect(bubbleSort(numbersOG)).toEqual(numbers);
     });
 
     it('can handle negative numbers', () => {
+        const numbersOG = [192, -41, -119, -143, 88, -63, 81, 140, 87, 11, -63, -19];
         const numbers = [192, -41, -119, -143, 88, -63, 81, 140, 87, 11, -63, -19];
+        numbers.sort((a, b) => a - b);
 
-        const sorted = numbers.sort((a, b) => a - b);
-
-        expect(bubbleSort(numbers)).toEqual(sorted);
+        expect(bubbleSort(numbersOG)).toEqual(numbers);
     });
 });
 
 describe('insertionSort', () => {
     it('sorts a numeric list of positive numbers', () => {
+        const numbersOG = [131, 174, 5, 10, 145, 143, 63, 108, 170, 81, 170, 5];
         const numbers = [131, 174, 5, 10, 145, 143, 63, 108, 170, 81, 170, 5];
-        const sorted = numbers.sort((a, b) => a - b);
+        numbers.sort((a, b) => a - b);
 
-        expect(insertionSort(numbers)).toEqual(sorted);
+        expect(insertionSort(numbersOG)).toEqual(numbers);
     });
 
     it('can handle negative numbers', () => {
+        const numbersOG = [192, -41, -119, -143, 88, -63, 81, 140, 87, 11, -63, -19];
         const numbers = [192, -41, -119, -143, 88, -63, 81, 140, 87, 11, -63, -19];
+        numbers.sort((a, b) => a - b);
 
-        const sorted = numbers.sort((a, b) => a - b);
-
-        expect(insertionSort(numbers)).toEqual(sorted);
+        expect(insertionSort(numbersOG)).toEqual(numbers);
     });
 });
 
 describe('quickSort', () => {
     it('sorts a numeric list of positive numbers', () => {
+        const numbersOG = [131, 174, 5, 10, 145, 143, 63, 108, 170, 81, 170, 5];
         const numbers = [131, 174, 5, 10, 145, 143, 63, 108, 170, 81, 170, 5];
-        const sorted = numbers.sort((a, b) => a - b);
+        numbers.sort((a, b) => a - b);
 
-        expect(quickSort(numbers)).toEqual(sorted);
+        expect(quickSort(numbersOG)).toEqual(numbers);
     });
 
     it('can handle negative numbers', () => {
+        const numbersOG = [192, -41, -119, -143, 88, -63, 81, 140, 87, 11, -63, -19];
         const numbers = [192, -41, -119, -143, 88, -63, 81, 140, 87, 11, -63, -19];
+        numbers.sort((a, b) => a - b);
 
-        const sorted = numbers.sort((a, b) => a - b);
-
-        expect(quickSort(numbers)).toEqual(sorted);
+        expect(quickSort(numbersOG)).toEqual(numbers);
     });
 });
 
 describe('mergeSort', () => {
     it('sorts a numeric list of positive numbers', () => {
+        const numbersOG = [131, 174, 5, 10, 145, 143, 63, 108, 170, 81, 170, 5];
         const numbers = [131, 174, 5, 10, 145, 143, 63, 108, 170, 81, 170, 5];
-        const sorted = numbers.sort((a, b) => a - b);
+        numbers.sort((a, b) => a - b);
 
-        expect(mergeSort(numbers)).toEqual(sorted);
+        expect(mergeSort(numbersOG)).toEqual(numbers);
     });
 
     it('can handle negative numbers', () => {
+        const numbersOG = [192, -41, -119, -143, 88, -63, 81, 140, 87, 11, -63, -19];
         const numbers = [192, -41, -119, -143, 88, -63, 81, 140, 87, 11, -63, -19];
+        numbers.sort((a, b) => a - b);
 
-        const sorted = numbers.sort((a, b) => a - b);
-
-        expect(mergeSort(numbers)).toEqual(sorted);
+        expect(mergeSort(numbersOG)).toEqual(numbers);
     });
 });
